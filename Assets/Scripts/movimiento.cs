60820935ee594f9b6367abe2fe56f82e536b6e38
Enzo Tomas <131463329+Ailin410@users.noreply.github.com>
2025-09-24 12:28:24 -0300

    Código inicial, y suma al código estamina

--- Assets/Scripts/movimiento.cs
+++ Assets/Scripts/movimiento.cs
@@ -17,11 +17,29 @@ public class StaminaSystem
         stamina = max;
         bajaStamina = baja;
         subeStamina = sube;
-    }
 
-    //Interfaz de la camara
-    //Con esta interfaz, el jugador depende de esta y no de un metodo en concreto
-    public interface ICameraController
+        public float CurrentStamina => stamina; //Devuelve cuanta estamina tiene
+        public bool HasStamina => stamina > 0f; //Devuelve verdadero si hay estamina, sino devuelve falso
+
+        public void Baja(float deltaTime)
+        {
+             //Resta estamina y si baja de cero lo corrigue y lo deja en 0
+            stamina -= bajaStamina * deltaTime;
+            if (stamina < 0f) stamina = 0f;
+        }
+
+        public void Sube(float deltaTime)
+        {
+            //Suma estamina y cuando supera el maximo se corrigue y lo deja en el valor maximo
+            stamina += subeStamina * deltaTime;
+            if (stamina > maxStamina) stamina = maxStamina;
+        }
+
+}
+
+//Interfaz de la camara
+//Con esta interfaz, el jugador depende de esta y no de un metodo en concreto
+public interface ICameraController
     {
         void Look(Transform playerTransform, Camera playerCamera);
     }
@@ -51,9 +69,66 @@ public class StaminaSystem
     }
 
 
-    //Movimiento inicial
+    //Codigo base
     public class movimiento : MonoBehaviour
 {
-   
+        public float walkSpeed = 2f;
+        public float runSpeed = 6f;
 
-}
+        public float maxStamina = 50f;
+        public float staminaDism = 10f;
+        public float staminaRegen = 5f;
+
+        public Camera playerCamera;
+        public float mouseSensitivity = 0.2f;
+        private Rigidbody rb;
+        private Vector2 moveInput;
+        private StaminaSystem staminaSystem;
+        private ICameraController cameraController;
+
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+
+            // Stamina (SRP)
+            staminaSystem = new StaminaSystem(maxStamina, staminaDism, staminaRegen);
+
+            // Cámara (DIP)
+            cameraController = new MouseCameraController(mouseSensitivity);
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        private void Update()
+        {
+            // Usamos la abstracción
+            cameraController.Look(transform, playerCamera);
+            
+        }
+
+        private void FixedUpdate()
+        {
+            bool isRunning = Keyboard.current.shiftKey.isPressed && staminaSystem.HasStamina;
+            float currentSpeed = isRunning ? runSpeed : walkSpeed;
+
+            if (isRunning)
+                staminaSystem.Baja(Time.fixedDeltaTime);
+            else
+                staminaSystem.Sube(Time.fixedDeltaTime);
+
+            Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+            if (move.magnitude > 1f) move.Normalize();
+
+            rb.MovePosition(rb.position + move * currentSpeed * Time.fixedDeltaTime);
+        }
+
+        public void OnMove(InputValue value)
+        {
+            moveInput = value.Get<Vector2>();
+        }
+
+
+
+    }
